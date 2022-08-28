73cc4ad41ce4089e3af585afaa6251aa0c7b4417
abanoub nagib <abanoub.nagib@hotmail.com>
2022-08-28 13:55:48 -0700

    mvp

--- index.js
+++ index.js
@@ -15,10 +15,23 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+this.name = name;
+this.age = age;
+this.stomach = [];
+}
+Person.prototype.eat = function(edible){
+  if(this.stomach.length< 10){
+    this.stomach.push(edible);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
 
 }
-
+Person.prototype.toString = function(){
+  return`${this.name}, ${this.age}`;
+}
 
 /*
   TASK 2
@@ -36,10 +49,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
 }
-
+Car.prototype.fill = function(gallons){
+this.tank = this.tank + gallons;
+}
 
 /*
   TASK 3
@@ -49,18 +67,21 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+Person.call(this, name, age);
+this.favoriteToy = favoriteToy;
 }
-
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `playig with ${this.favoriteToy}`;
+}
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. window binging: When none of the other rules apply, this will return the window or global object in node or undefined in strict mode.
+  2. Implicit biding: when the function is invoked look to thee left of the dot thats what this refers to.
+  3. explicit biding: .call, .apply, .bind
+  4. new binding: when a function is created as a constructo this point to the newly create object
 */
 
 ///////// END OF CHALLENGE /////////
